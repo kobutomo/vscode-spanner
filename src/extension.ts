05d47843f5a09b1987a8df69c4f226c41d85a853
ntomoro <tomoro.nishida@m-hand.com>
2019-04-22 14:55:24 +0900

    コマンド追加

--- src/extension.ts
+++ src/extension.ts
@@ -8,24 +8,51 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "spanner" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.spanner', () => {
-		vscode.window.showInformationMessage('Hello');
+		vscode.window.showInformationMessage('input command and press enter');
 		const editor = vscode.window.activeTextEditor;
 		const getInput = async () => {
 			const input = await vscode.window.showInputBox();
-			console.log(input);
-			if (editor && editor.selection.start && editor.selection.end) {
+			let newString = "";
+			if (editor && editor.selection.start && editor.selection.end && input) {
 				const selectRange = new vscode.Range(editor.selection.start, editor.selection.end);
-				const select = editor.document.getText(selectRange);
-				let newString = "";
-				console.log(select);
-				for (let i = 0; i < select.length; i++) {
-					newString += '<span>' + select[i] + '</span>';
+				let select = editor.document.getText(selectRange);
+				switch (input) {
+					case "span": {
+						for (let i = 0; i < select.length; i++) {
+							newString += '<span>' + select[i] + '</span>';
+						}
+						break;
+					}
+					case "ul": {
+						select = select.replace(/\r/g, "");
+						const arr = select.split("\n");
+						newString = `<ul class="list">`;
+						arr.forEach((str) => {
+							newString += `\n<li class="item">${str}</li>`;
+						});
+						newString += "\n</ul>";
+						break;
+					}
+					case "ol": {
+						select = select.replace(/\r/g, "");
+						const arr = select.split("\n");
+						newString = `<ol class="list">`;
+						arr.forEach((str) => {
+							str.replace("\n","");
+							newString += `\n<li class="item">${str}</li>`;
+						});
+						newString += "\n</ol>";
+						break;
+					}
+					default: {
+						vscode.window.showInformationMessage('aaa');
+						break;
+					}
 				}
 				editor.edit((builder) => {
 					builder.replace(selectRange, newString);
